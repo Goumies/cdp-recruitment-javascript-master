4fa78c7a07ad834757f0e0de9f0f0768984e9084
Romy Alula <goumiesone.dev@gmail.com>
2021-03-12 12:28:24 +0100

    Implement Count

--- src/count/count.js
+++ src/count/count.js
@@ -1,6 +1,34 @@
+const { Readable } = require('stream')
+const { data } = require('../../data')
+
+let out = []
+
 function count(){
-    console.info('Count!')
-    return 'Count!'
+    const inCountStream = new Readable({
+        read(size) {
+            for (const country of data) {
+                const { people } = country
+
+                out.push({name : `${country.name} [${people.length}]`, people: []});
+
+                for (const person of people) {
+                    const { animals } = person
+
+                    const lastElementIndex = out.length-1;
+                    const lastStoredCountry = out[lastElementIndex];
+
+                    lastStoredCountry.people = [
+                        ...lastStoredCountry.people, {name: `${person.name} [${animals.length}]`, animals}
+                    ]
+                }
+            }
+            console.dir(out, {
+                depth: null,
+            })
+        }
+    })
+    inCountStream.read()
+    return inCountStream
 }
 
 module.exports = count
