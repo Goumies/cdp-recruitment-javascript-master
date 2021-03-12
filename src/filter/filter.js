ac2b2c510116d98b5d2b4625d7d94b4328484ae5
Romy Alula <goumiesone.dev@gmail.com>
2021-03-12 12:28:06 +0100

    Implement Filter

--- src/filter/filter.js
+++ src/filter/filter.js
@@ -1,8 +1,35 @@
-const { data } = require("../../data");
+const { Readable } = require('stream')
+const { data } = require('../../data')
 
-function filter(pattern){
-    console.info('Filter!')
-    return 'Filter!'
+let pattern = ''
+const out = []
+
+function filter(receivedPattern){
+    pattern = receivedPattern
+    const inFilterStream = new Readable({
+        read(size) {
+            for (const country of data) {
+                const { people } = country
+
+                for (const person of people) {
+                    const { animals } = person
+
+                    animals
+                        .filter(animal => animal.name.includes(pattern))
+                        .forEach(result =>
+                            result && out.push(
+                                {name : country.name, people: [{name: person.name, animals: [result]}]}
+                            )
+                        );
+
+                }
+            }
+            console.dir(out, {
+                depth: null,
+            })
+        }
+    })
+    inFilterStream.read()
+    return inFilterStream
 }
-
 module.exports = filter
--- src/filter/filter.test.js
+++ src/filter/filter.test.js
@@ -1,7 +1,39 @@
-const filter = require('./filter')
+const filter = require("./filter");
 
-describe('filter', () => {
-    test('should return Filter!', () => {
-        expect(filter()).toBe('Filter!')
+describe('Filter', () => {
+    test('should return Uzuzozne and Satanwi countries, given the ry pattern', () => {
+        const COUNTRIES_WITH_ANIMALS_WITH_GIVEN_PATTERN = [
+            {
+                name: 'Uzuzozne',
+                people: [
+                    {
+                        name: 'Lillie Abbott',
+                        animals: [
+                            {
+                                name: 'John Dory'
+                            }
+                        ]
+                    }
+                ]
+            },
+            {
+                name: 'Satanwi',
+                people: [
+                    {
+                        name: 'Anthony Bruno',
+                        animals: [
+                            {
+                                name: 'Oryx'
+                            }
+                        ]
+                    }
+                ]
+            }
+        ];
+
+        filter('ry')
+            .on('data', (data) =>{
+                expect(data).toStrictEqual(COUNTRIES_WITH_ANIMALS_WITH_GIVEN_PATTERN)
+            })
     })
 })
